315784f64801d53fce58b7a0f60982bcbd70527e
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2022-11-20 17:09:33 +0100

    Connecting to mongoDb

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,11 +1,32 @@
+import * as dotenv from "dotenv";
 import express, {Express, Request, Response} from 'express';
+import {connect, ConnectOptions, connection} from 'mongoose';
 
-const app: Express = express();
+dotenv.config();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Typescrit and node works')
-})
+(async () => {
+    connection.once("open", (_) => {
+        console.log("Connected to the database");
+      });
+    
+    connection.on("error", (err) => {
+      console.error("Database connection error:", err);
+      process.exit(1);
+    });
 
-app.listen(4321, () => {
-    console.log('Running on 4321');
-})
+    await connect(process.env.MONGO_URI as string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions);
+
+    
+    const app: Express = express();
+    
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Typescrit and node works')
+    })
+    
+    app.listen(4321, () => {
+        console.log('Running on 4321');
+    })
+})()
