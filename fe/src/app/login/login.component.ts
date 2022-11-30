a0541557bfc32243e90b9ae51fe8271d586f1ceb
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2022-11-30 15:56:00 +0100

    Creating signUp, login components

--- fe/src/app/login/login.component.ts
+++ fe/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CredentialsError } from '../errors/credentialsError';
+import { FormError } from '../errors/formError';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
+  @ViewChild('loginForm') loginForm: NgForm | undefined;
+
+  constructor(private authService: AuthService) {
+    
+  }
+
+  login(formData: any) {
+    this.authService.login(formData)
+    .subscribe({
+      error: (error) => {
+        console.log("login component error:", error)
+        if (error instanceof FormError) {
+          error.originalError.forEach((e: any) => {
+            e.constraints.forEach((c: Object) => {
+              this.loginForm?.controls[e.property].setErrors(c)
+            });
+          });
+        } else if (error instanceof CredentialsError) {
+          this.loginForm?.form.setErrors({
+            invalidCredentials: error.originalError.message
+          })
+        } else {
+          throw error;
+        }
+      }
+    })
+  }
 }
---
+++ fe/src/app/signup/signup.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, NgForm } from '@angular/forms';
+import { FormError } from '../errors/formError';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  @ViewChild('signUpForm') signUpForm: NgForm | undefined;
+
+  constructor(private authService: AuthService) {
+  }
+
+  signUp(formData: any) {
+    this.authService.signUp(formData)
+    .subscribe({
+      error: (error) => {
+        console.log("signup component error:", error)
+        if (error instanceof FormError) {
+          error.originalError.forEach((e: any) => {
+            e.constraints.forEach((c: Object) => {
+              this.signUpForm?.controls[e.property].setErrors(c)
+            });
+          });
+        } else {
+          throw error;
+        }
+      }
+    })
+  }
+}
