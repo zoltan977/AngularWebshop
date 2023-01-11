14ef9e2a8f079faadc26152073f94dd60cd19743
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-01-11 20:30:05 +0100

    Creating the shopping-cart component

--- fe/src/app/services/shopping-cart.service.ts
+++ fe/src/app/services/shopping-cart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, catchError, of, Observable } from 'rxjs';
+import { tap, catchError, of, Observable, switchMap } from 'rxjs';
 import { AppError } from '../errors/appError';
 import { Product } from '../models/product-model';
 import { ShoppingCart } from '../models/shopping-cart';
@@ -64,6 +64,25 @@ export class ShoppingCartService {
       tap((cart: ShoppingCart) => {
         console.log("shopping-cart service response data", cart);
         this.currentShoppingCart = new ShoppingCart(cart);
+      }),
+      catchError(serviceErrorHandler)
+      )
+  }
+
+  clearCart(): Observable<ShoppingCart | AppError> {
+    const cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      return of(new ShoppingCart())
+    }
+    const response = this.httpClient.delete<boolean>(this.PATH + '/clear/' + cartId);
+
+    return response.pipe(
+      switchMap((data: boolean) => {
+        console.log("shopping-cart service response data", data);
+        this.currentShoppingCart = new ShoppingCart();
+        localStorage.removeItem('cartId');
+
+        return of(this.currentShoppingCart)
       }),
       catchError(serviceErrorHandler)
       )
