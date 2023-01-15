e07cd0aa5efb157c9ae46b3da0b6dbf5ad91b5c3
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-01-15 21:29:58 +0100

    Creating my-orders component

--- fe/src/app/components/my-orders/my-orders.component.ts
+++ fe/src/app/components/my-orders/my-orders.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { OrderService, OrderWithDate } from 'src/app/services/order.service';
 
 @Component({
   selector: 'my-orders',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-orders.component.scss']
 })
 export class MyOrdersComponent {
+  dataSource!: MatTableDataSource<OrderWithDate>;
+  displayedColumns: string[] = ['userEmail', 'name', 'dateCreated', '_id'];
+  @ViewChild(MatSort) sort!: MatSort;
 
+  constructor(private orderService: OrderService) {}
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnInit(): void {
+    this.populateMatTableWithProducts();
+  }
+
+  private populateMatTableWithProducts() {
+    this.orderService.getAllByUser()
+    .subscribe({
+      next: (data) => {
+        this.dataSource = new MatTableDataSource(data as OrderWithDate[]);
+        this.dataSource.sort = this.sort;
+
+        this.settingMatTableFilter();
+      },
+      error: (error) => {
+        console.log("admin orders component error:", error)
+        throw error;
+      }
+    })
+  }
+
+  private settingMatTableFilter() {
+    this.dataSource.filterPredicate = function (record,filter) {
+      return record.userEmail.toLowerCase().includes(filter.toLowerCase()) || 
+      record.name.toString().toLowerCase().includes(filter.toLowerCase());
+    }
+  }
 }
