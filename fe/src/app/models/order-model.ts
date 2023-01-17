3cc14a09bd37660553ada16ddd485f22473b4dff
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-01-17 16:28:47 +0100

    Creating order-details-component and adding get method to order-service

--- fe/src/app/models/order-model.ts
+++ fe/src/app/models/order-model.ts
@@ -2,6 +2,13 @@ import { prop, required } from "@rxweb/reactive-form-validators";
 import { ShoppingCart } from "./shopping-cart";
 
 export class Order {
+    constructor(init?: Order) {
+        if (init) {
+            Object.assign(this, init)
+            this.cart = new ShoppingCart(init.cart)
+        }
+    }
+
     _id!: string;
 
     @required()
@@ -18,4 +25,16 @@ export class Order {
     
     @prop()
     cart!: ShoppingCart;
+}
+export class OrderWithDate extends Order {
+    public readonly dateCreated: Date | undefined;
+
+    constructor(init?: OrderWithDate) {
+        if (init) {
+            super(init);
+            this.dateCreated = init?.dateCreated;
+        } else {
+            super();
+        }
+    }
 }
