d0d7175c9fcbc86713956f710df730b8de5888e2
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-01-25 00:36:09 +0100

    Adding delete-product-dialog to product-form component

---
+++ fe/src/app/components/admin/admin-products/product-form/delete-product-dialog/delete-product-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'delete-product-dialog',
+  templateUrl: './delete-product-dialog.component.html',
+  styleUrls: ['./delete-product-dialog.component.scss']
+})
+export class DeleteProductDialogComponent {
+
+}
